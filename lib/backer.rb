ba04f232f3f1c6324b08ea88da37df13387821bb
Vincent Servio <vincentservio@gmail.com>
2020-02-25 05:36:06 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Backer 
+  attr_accessor :backed_projects 
+  attr_reader :name 
+  def initialize(name) 
+    @name = name 
+    @backed_projects = []
+     @backers = []
+  end 
+  
+   def back_project(project)
+     @backed_projects << project
+     
+    # binding.pry
+   end
+   
+end 
---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project 
+  attr_accessor :backers
+  attr_reader :title 
+ 
+  def initialize(title) 
+  @title = title
+    @backers = []
+    @backed_projects = []
+end 
+    def add_backer(backer)
+      @backers << backer
+    end
+end 
